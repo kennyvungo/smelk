f49a052d7e625075dc4ee373459bce0018571854
Kenny <kennyvungo@gmail.com>
2023-07-03 13:47:21 -0700

    set up User Model, currently not working, creating user in Postman gives 404

---
+++ backend/models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const userSchema = new Schema({
+    username: {
+      type: String,
+      required: true
+    },
+    fname:{
+        type: String,
+        required: true
+    },
+    lname:{
+        type: String,
+        required: true
+    },
+    hashedPassword: {
+      type: String,
+      required: true
+    }
+  }, {
+    timestamps: true
+  });
+
+  module.exports = mongoose.model('User', userSchema);
