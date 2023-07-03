3a7f7a2bcb59ce3dcabb8f3ec7089282a618c003
Kenny <kennyvungo@gmail.com>
2023-07-03 10:14:13 -0700

    finished csurf and users router

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,37 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+// const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+const { isProduction } = require('./config/keys');
+const csurf = require('csurf');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// Express Routers
+const usersRouter = require('./routes/api/users');
+const csrfRouter = require('./routes/api/csrf');
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//SECURITY MIDDLEWARE
+if (!isProduction) {
+    app.use(cors());
+}
+app.use(
+    csurf({
+    cookie: {
+        secure: isProduction,
+        sameSite: isProduction && "Lax",
+        httpOnly: true
+        }
+    })
+);
 
+// attaching Express Routers
+app.use('/api/users', usersRouter);
+app.use('/api/csrf', csrfRouter);
 module.exports = app;
