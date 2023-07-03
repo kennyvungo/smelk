f6146b49530ccd71552c12a2411f0210fa38b200
Shanna Le <shannatle@gmail.com>
2023-07-03 14:46:55 -0700

    Session auth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,34 @@
+import { Switch } from 'react-router-dom';
+import { AuthRoute } from './components/Routes/Routes';
+import { Route, Redirect } from 'react-router-dom';
+
+import MainPage from './components/MainPage/MainPage';
+// import LoginForm from './components/SessionForms/LoginForm';
+// import SignupForm from './components/SessionForms/SignupForm';
+import NavBar from './components/NavBar/NavBar';
+
 function App() {
   return (
-    <h1>Hello from App</h1>
+    <>
+      <NavBar />
+      <Switch>
+
+        {/* <ProtectedRoute exact path="/event/edit" component={EventEditPage} /> */}
+        {/* <ProtectedRoute exact path="/event/new" component={EventFormPage} /> */}
+        {/* <ProtectedRoute exact path="/profile/:id" component={UserShowPage} /> */}
+
+        {/* <AuthRoute exact path="/event/:id" component={EventShowPage} /> */}
+        {/* <AuthRoute exact path="/signup" component={SignupForm} /> */}
+        {/* <AuthRoute exact path="/login" component={LoginForm} /> */}
+        <AuthRoute exact path="/" component={MainPage} />
+
+        <Route>
+          <Redirect to="/" />
+        </Route>
+
+      </Switch>
+      {/* <Footer /> */}
+    </>
   );
 }
 
