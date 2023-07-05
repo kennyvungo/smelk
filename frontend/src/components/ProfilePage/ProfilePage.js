de59cd3e9687686e5146a0ad385d876ca5561c7a
Shanna Le <shannatle@gmail.com>
2023-07-05 14:48:28 -0700

    Fix background image

--- frontend/src/components/ProfilePage/ProfilePage.js
+++ frontend/src/components/ProfilePage/ProfilePage.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchUserEvents } from '../../store/events';
 import { Link } from "react-router-dom";
@@ -15,7 +15,6 @@ function ProfilePage () {
     }, [dispatch, user]);
 
     if (!userEvents) return null;
-
 
     return (
         <>
