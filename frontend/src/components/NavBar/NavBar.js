929afd18afc07a97512604c175a2a8a9621faa1e
ertan3 <ernesttan96@gmail.com>
2023-07-05 10:19:35 -0700

    Create object after clicking submit on event form

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -19,8 +19,8 @@ function NavBar () {
         <div className="links-nav">
           <Link to={`/profile/${userId}`}>Profile</Link>
           {/* temp view event link to test need to update by grabbing the event object ID*/}
-          <Link to={`/event/64a36905031eb4776e7f543a`}>View Event1</Link> 
-          <Link to={`/event/64a36905031eb4776e7f543b`}>View Event2</Link> 
+          <Link to={`/event/64a4cfb265da3e4b1adde4cf`}>View Event1</Link> 
+          <Link to={`/event/64a4cfb265da3e4b1adde4d0`}>View Event2</Link> 
           <button onClick={logoutUser}>Logout</button>
         </div>
       );
