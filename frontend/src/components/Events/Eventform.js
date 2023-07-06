d420629e86295cdc4b3b1e8b46247dd52579c242
misha <mishabansal7@gmail.com>
2023-07-06 09:46:08 -0700

    adding calendar back

--- frontend/src/components/Events/Eventform.js
+++ frontend/src/components/Events/Eventform.js
@@ -1,29 +1,27 @@
-import React from 'react'
-import { useEffect,useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { createEvent } from '../../store/events';
-import { useHistory } from 'react-router-dom';
-import Calendar from '../Calendar/Calendar';
-
+import React from "react"
+import { useEffect,useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { createEvent } from "../../store/events";
+import { useHistory } from "react-router-dom";
+import Calendar from "../Calendar/Calendar";
 const EventForm = () => {
   const dispatch = useDispatch();
   const history = useHistory();
   const user = useSelector(state => state.session.user);
-
-  const [eventName, setEventName] = useState('');
-  const [eventDates, setEventDates] = useState('');
-  const [eventStartTime, setEventStartTime] = useState('12:00 AM');
-  const [eventEndTime, setEventEndTime] = useState('12:00 AM');
-  
+  const [eventName, setEventName] = useState("");
+  const [eventDates, setEventDates] = useState([]);
+  const [eventStartTime, setEventStartTime] = useState("12:00 AM");
+  const [eventEndTime, setEventEndTime] = useState("12:00 AM");
   const hours = Array.from({ length: 24 }, (v, i) => {
-    const period = i < 12 || i === 24 ? 'AM' : 'PM';
+    const period = i < 12 || i === 24 ? "AM" : "PM";
     const twelveHourFormat = i === 0 ? 12 : i < 13 ? i : i - 12;
     return `${twelveHourFormat}:00 ${period}`;
   });
-
+  const handleDatesChange = (dates) => {
+    setEventDates(dates); // Update the eventDates state with the selected dates from the Calendar component
+  };
   const handleSubmit = async (e) => {
     e.preventDefault();
-
     const newEvent = {
       owner: user._id,
       name: eventName,
@@ -31,41 +29,29 @@ const EventForm = () => {
       dailyEventStartTime: eventStartTime,
       dailyEventEndTime: eventEndTime,
     };
-
     const createdEvent = await dispatch(createEvent(newEvent));
-
     if (createdEvent) {
-      history.push('/event/' + createdEvent.event._id);
+      history.push("/event/" + createdEvent.event._id);
     }
   }
-
   return (
     <>
-      <form className='event-form' onSubmit={handleSubmit}>
+      <form className="event-form" onSubmit={handleSubmit}>
           <h2>Create Event</h2>
-          
-          <div className='eventerrors'></div>
-
+          <div className="eventerrors"></div>
           <label>
             <span>Event Name</span>
-            <input 
-              type="text" 
-              value={eventName} 
-              onChange={e => setEventName(e.target.value)} 
-            />
-          </label>
-          <br/>
-
-          <label>
-            <span>Dates Array:</span>
-            <input 
+            <input
               type="text"
-              value={eventDates}
-              onChange={e => setEventDates(e.target.value)} 
+              value={eventName}
+              onChange={e => setEventName(e.target.value)}
             />
           </label>
           <br/>
-
+          <label>
+            <Calendar onDatesChange={handleDatesChange}/>
+          </label>
+          <br/>
           <label>
             <span>Event Start time?</span>
             <select value={eventStartTime} onChange={e => setEventStartTime(e.target.value)}>
@@ -75,7 +61,6 @@ const EventForm = () => {
             </select>
           </label>
           <br/>
-          
           <label>
             <span>Event End time?</span>
             <select value={eventEndTime} onChange={e => setEventEndTime(e.target.value)}>
@@ -85,7 +70,7 @@ const EventForm = () => {
             </select>
           </label>
           <br/>
-          <input 
+          <input
             type="submit"
             value="Create Event"
           />
@@ -93,5 +78,4 @@ const EventForm = () => {
     </>
   )
 }
-
 export default EventForm;
