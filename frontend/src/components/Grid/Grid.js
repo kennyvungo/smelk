d955321badd946c4b988d0e7ee03255fee474af3
Kenny <kennyvungo@gmail.com>
2023-07-06 14:30:10 -0700

    merging again

--- frontend/src/components/Grid/Grid.js
+++ frontend/src/components/Grid/Grid.js
@@ -1,20 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import './Grid.css';
-<<<<<<< HEAD
-//helper function to convert time
-const convertTo12HourFormat = (time) => {
-    let hour = Math.floor(time);
-    let minute = (time % 1) > 0 ? 30 : 0;
-    let amPm = hour >= 12 ? 'PM' : 'AM';
-    if (hour === 0) {
-        hour = 12;
-    } else if (hour > 12) {
-        hour -= 12;
-    }
-    return `${hour}:${minute === 0 ? '00' : '30'} ${amPm}`;
-}
-=======
->>>>>>> agggrid
 function Grid({ event }) {
     const [grid, setGrid] = useState({});
     useEffect(() => {
@@ -37,7 +22,6 @@ function Grid({ event }) {
         newGrid[date][time] = !newGrid[date][time];
         setGrid(newGrid);
     };
-<<<<<<< HEAD
     const columnDates = Object.keys(grid);
     let second;
     if (grid) {
@@ -51,22 +35,11 @@ function Grid({ event }) {
         }
     }
     // console.log(grid);
-=======
-    console.log("THIS IS A REGULAR GRID",grid)
->>>>>>> agggrid
     if (grid) {
         return (
             <div className='grid'>
                 {Object.entries(grid).map(([date, timeSlots]) => (
                     <div className='grid-row' key={date}>
-<<<<<<< HEAD
-                        {Object.entries(timeSlots).map(([time, selected]) => (
-                            <div
-                                className='grid-cell'
-                                key={time}
-                                onClick={() => handleTimeSlotClick(date, time)}
-                                style={{backgroundColor: selected ? '#FFD1DC' : '#FFF9B0'}}
-=======
                         <div className='date-header'>{date}</div>
                         <div className='date-header'>{getDayOfWeek(date)} </div>
                         {Object.entries(timeSlots).map(([time, selected]) => (
@@ -75,7 +48,6 @@ function Grid({ event }) {
                             key={time}
                             onClick={() => handleTimeSlotClick(date, time)}
                             style={{backgroundColor: selected ? '#A98DE2' : '#CBC3E3'}}
->>>>>>> agggrid
                             >
                                 {time}
                             </div>
@@ -89,31 +61,6 @@ function Grid({ event }) {
     }
 }
 export default Grid;
-<<<<<<< HEAD
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-=======
 //helper function to convert time
 const convertTo12HourFormat = (time) => {
     let hour = Math.floor(time);
@@ -131,4 +78,3 @@ const getDayOfWeek = (date) => {
     const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
     return days[new Date(date).getDay()];
 }
->>>>>>> agggrid
