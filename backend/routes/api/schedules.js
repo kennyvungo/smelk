4d014341c290fc62bab8de8edbc83b8974467c60
Kenny <kennyvungo@gmail.com>
2023-07-05 20:38:39 -0700

    added route for aggregating

--- backend/routes/api/schedules.js
+++ backend/routes/api/schedules.js
@@ -22,4 +22,112 @@ router.get('/', async (req, res) => {
     return res.json(schedules)
 })
 
+router.get('/agg',async(req,res,next) => {
+    try{
+        const agg = await Schedule.aggregate([
+            {
+              '$addFields': {
+                'dateArray': {
+                  '$objectToArray': '$dailySchedule'
+                }
+              }
+            }, {
+              '$unwind': {
+                'path': '$dateArray'
+              }
+            }, {
+              '$addFields': {
+                'timeArray': {
+                  '$objectToArray': '$dateArray.v'
+                }
+              }
+            }, {
+              '$unwind': {
+                'path': '$timeArray'
+              }
+            }, {
+              '$group': {
+                '_id': {
+                  'event': '$eventId', 
+                  'date': '$dateArray.k', 
+                  'time': '$timeArray.k'
+                }, 
+                'available': {
+                  '$push': {
+                    '$cond': [
+                      {
+                        '$eq': [
+                          '$timeArray.v', true
+                        ]
+                      }, '$firstName', null
+                    ]
+                  }
+                }, 
+                'unavailable': {
+                  '$push': {
+                    '$cond': [
+                      {
+                        '$eq': [
+                          '$timeArray.v', false
+                        ]
+                      }, '$firstName', null
+                    ]
+                  }
+                }
+              }
+            }, {
+              '$group': {
+                '_id': {
+                  'event': '$_id.event', 
+                  'date': '$_id.date'
+                }, 
+                'count': {
+                  '$sum': 1
+                }, 
+                'times': {
+                  '$push': {
+                    'k': '$_id.time', 
+                    'v': {
+                      'available': '$available', 
+                      'unavailable': '$unavailable'
+                    }
+                  }
+                }
+              }
+            }, {
+              '$project': {
+                'times': {
+                  '$arrayToObject': '$times'
+                }
+              }
+            }, {
+              '$group': {
+                '_id': '$_id.event', 
+                'dates': {
+                  '$push': {
+                    'k': '$_id.date', 
+                    'v': '$times'
+                  }
+                }
+              }
+            }, {
+              '$project': {
+                'dates': {
+                  '$arrayToObject': '$dates'
+                }
+              }
+            }
+          ])
+          return res.json(agg)
+    }
+    catch(err){
+        const error = new Error('Schedule not found');
+        error.statusCode=404;
+        error.errors = {message: "no schedule found with that id"};
+        return next(error);
+    }
+
+    
+})
+
 module.exports = router;
